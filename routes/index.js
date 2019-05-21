1472068262c7ef8502e5257dc2af181902f48d41
Mike Feldberg <mikefeldberg@gmail.com>
2019-05-20 19:20:05 -0700

    fix undefined category crash

--- routes/index.js
+++ routes/index.js
@@ -16,15 +16,25 @@ router.post('/', function (req, res, next) {
     // console.log(req)
     // console.log('REQ.BODY BELOW')
     // console.log(req.body)
-    console.log('REQ.BODY.CATEGORY BELOW')
-    console.log(req.body.category)
+    // console.log('REQ.BODY.CATEGORY BELOW')
+    // console.log(req.body.category)
     request(catURL, function (err, response, body) {
         var categories = JSON.parse(body)
         // console.log('BODY BELOW')
         // console.log(body)
         // console.log('CATEGORIES BELOW')
         // console.log(categories)
-        request(rootURL + '?category=' + req.body.category, function (err, response, body) {
+        var category = req.body.category
+        console.log('CATEGORY')
+        console.log(category)
+        var url = rootURL
+        console.log('URL')
+        console.log(url)
+        if (category) {
+            url = rootURL + '?category=' + category;
+        }
+
+        request(url, function (err, response, body) {
             var joke = JSON.parse(body);
             res.render('index', { joke: joke.value, categories })
         });
